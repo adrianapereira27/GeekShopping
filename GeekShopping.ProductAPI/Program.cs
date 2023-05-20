8a27c2ea514fc4c6aef2419d45bd5a8d60439999
Adriana <pereira.adri@gmail.com>
2023-05-19 22:11:36 -0300

    Configurações de banco

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,11 +1,25 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration["MySqlConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(
+    connection, 
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "1.0" }); 
+});
 
 var app = builder.Build();
 
